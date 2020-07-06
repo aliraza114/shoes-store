4a32ced0c91a5e7d6b9349549977f36a3f3562c8
aliraza114 <aliraza3569@gmail.com>
2020-07-06 12:38:00 +0500

    router setup

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function About(){
+    return(
+        <div>
+            Hello About
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Home(){
+    return(
+        <div>
+            Hello Home
+        </div>
+    )
+}
+
+export default Home
