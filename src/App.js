00b974c62f8a76f55ea96708b8234bc794a148af
aliraza114 <aliraza3569@gmail.com>
2020-07-16 09:48:16 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Home from './pages/Home'
 import About from './pages/About'
+import Cart from './pages/Cart'
+import AppBar from './AppBar'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-     <h1>
-        Welcome
-     </h1>
+     <AppBar></AppBar>
      <Routes>
        <Route path="/" element={<Home/>}></Route>
+       <Route path="cart" element={<Cart/>}></Route>
        <Route path="about" element={<About/>}></Route>
      </Routes>
     </div>
